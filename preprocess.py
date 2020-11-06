5fb8d439c9d50d17734e80a2745aa66a98ffdb3d
Vinícius <vinicius.matheus252@gmail.com>
2020-11-06 10:59:10 -0300

    Init

---
+++ preprocess.py
@@ -0,0 +1,28 @@
+import re
+import argparse
+import json
+
+def preprocess_lyric(lyric: str) -> str:
+    text = lyric.lower().strip()
+    text = re.sub(r'([.:?!+(),]+)', r' \1 ', text)
+    text = re.sub(r'(^|(?<=\W))(-)(?=\w)', r' \2 ', text)
+    text = re.sub(r' +', ' ', text)
+    return text.strip()
+
+if __name__ == '__main__':
+    parser =  argparse.ArgumentParser()
+    parser.add_argument('json_file', help='JSON file from scrapper', type=str)
+    parser.add_argument('out_file', help='Output file', type=str)
+    args = parser.parse_args()
+
+    with open(args.json_file, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+        with open(args.out_file, 'w', encoding='utf-8') as out_file:
+            for d in data:
+                lyrics = ""
+                for v in d['verses']:
+                    if len(v.strip()) > 0:
+                        lyrics += preprocess_lyric(v) + " \\n "
+                
+                out_file.write(lyrics.strip() + "\n")
---
+++ scrapper.py
@@ -0,0 +1,55 @@
+import sys
+import re
+import json
+from dataclasses import dataclass, asdict
+from typing import List
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+
+base_url = 'https://www.letras.mus.br'
+artist_url = f'{base_url}/%s/mais-tocadas.html'
+
+@dataclass
+class Song:
+    name: str
+    verses: List[str]
+
+def get_songs(artist: str) -> List[Song]:
+    html = urlopen(artist_url % (artist))
+    bs = BeautifulSoup(html, 'html.parser')
+    lyrics = []
+
+    for link in bs.find('ul', {'class': 'cnt-list-songs'}).find_all('a'):
+        if 'href' in link.attrs:
+            song_name = link.find('span').getText().strip()
+            
+            print(song_name)
+
+            verses = get_page_verses(link.attrs['href'])
+
+            lyrics.append(Song(song_name, verses))
+
+    return lyrics
+
+
+def get_page_verses(page) -> List[str]:
+    html = urlopen(f"{base_url}/{page}")
+    bs = BeautifulSoup(html, 'html.parser')
+    verses = []
+
+    for verse in bs.find('div', {'class': 'cnt-letra p402_premium'}).find_all('p'):
+        p_removed = re.sub(r'(<p>)|(</p>)', "", str(verse))
+        br_removed = re.sub(r'<[^<]*br[^>]*>', "\n", p_removed).strip()
+        
+        for v in br_removed.split("\n"):
+            if len(v.split()) > 0:
+                verses.append(v)
+    
+    return verses
+
+if __name__ == "__main__":
+    lyrics = get_songs(sys.argv[1])
+    
+    with open(f"{sys.argv[1]}.json", "w", encoding="utf-8") as o:
+        json.dump([asdict(l) for l in lyrics], o, ensure_ascii=False)
